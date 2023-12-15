8f3c1ad153d69adb530f02b67a4fd1bcdef5ce1d
ken-cherry <ken@cherrywebdev.com>
2023-12-14 21:43:39 -0600

    add img

--- js/footer.js
+++ js/footer.js
@@ -16,19 +16,21 @@ const social = [
   },
 ];
 
-// const socialMedia = document.querySelector(".social-icons");
+const socialMedia = document.querySelector(".social-icons");
 
-// window.addEventListener("DOMContentLoaded", function () {
-//   let displayIcons = social.map(function (item) {
-//     console.log(item);
-//     return `<ul class="icons">
-//                 <li>
-//                     <a href=${item.link} class=${item.icon} target="_blank"></a>
-//                 </li>
-//             </ul>`;
-//   });
-//   displayIcons = displayIcons.join("");
-//   console.log(displayIcons);
+window.addEventListener("DOMContentLoaded", function () {
+  let displayIcons = social.map(function (item) {
+    console.log(item);
 
-//   socialMedia.innerHTML = displayIcons;
-// });
+    return `<ul class="icons">
+                <li>
+                    <a href=${item.link} class=${item.icon} target="_blank"></a>
+                </li>
+            </ul>`;
+  });
+  displayIcons = displayIcons.join("");
+  console.log(displayIcons);
+
+  //   socialMedia.innerHTML = displayIcons;
+  socialMedia.innerHTML = displayIcons;
+});
